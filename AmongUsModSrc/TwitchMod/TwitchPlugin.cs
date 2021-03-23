3bc6794897577fc251554ff6b4b9d6d77a0a3944
Efren Aguilar <efrenaguilar95@hotmail.com>
2021-03-22 22:37:33 -0700

    Revamp Custom Colors
    Its about the same amount of code as before, but I like this WAY more.

--- AmongUsModSrc/TwitchMod/TwitchPlugin.cs
+++ AmongUsModSrc/TwitchMod/TwitchPlugin.cs
@@ -23,92 +23,9 @@ namespace TestMod
         {
             Name = Config.Bind("Fake", "Name", "SUSMAN");
 
-            //Really janky way of adding the custom twitch color. If anyone knows a better way, please do.
+            CustomColorLoader.AddCustomColorsToGame();
 
-            //this are the short names
-            string[] shortColorNames =
-            {
-			//we need to add the vanilla colors so that the new color won't replace it
-			"RED",
-            "DBLUE",
-            "GRN",
-            "PINK",
-            "ORNG",
-            "YLOW",
-            "BLAK",
-            "WHTE",
-            "PURP",
-            "BRWN",
-            "CYAN",
-            "LIME",
-			//The twitch color
-			"TWCH",
-
-            };
-            //this are the long names
-            string[] colorNames =
-            {
-            "Red",
-            "Dark Blue",
-            "Green",
-            "Pink",
-            "Orange",
-            "Yellow",
-            "Black",
-            "White",
-            "Purple",
-            "Brown",
-            "Cyan",
-            "Lime",
-			//The Twitch color(with full name)
-			"Twitch"
-
-        };
-            Color32[] playerColors =
-            {
-				//this one are the colors
-			new Color32(240, 19, 19, byte.MaxValue),
-            new Color32(19, 46, 210, byte.MaxValue),
-            new Color32(0, 92, 23, byte.MaxValue),
-            new Color32(238, 84, 187, byte.MaxValue),
-            new Color32(250, 167, 0, byte.MaxValue),
-            new Color32(246, 246, 87, byte.MaxValue),
-            new Color32(63, 71, 78, byte.MaxValue),
-            new Color32(215, 225, 241, byte.MaxValue),
-            new Color32(107, 47, 188, byte.MaxValue),
-            new Color32(77, 50, 21, byte.MaxValue),
-            new Color32(56, 254, 220, byte.MaxValue),
-            new Color32(80, 240, 57, byte.MaxValue),
-			//The Twitch color
-			//new Color32(145, 71, 255, byte.MaxValue),
-            new Color32(164, 108, 243, byte.MaxValue),
-            };
-            Color32[] shadowColors =
-            {
-            new Color32(122, 8, 56, byte.MaxValue),
-            new Color32(9, 21, 142, byte.MaxValue),
-            new Color32(0, 56, 14, byte.MaxValue),
-            new Color32(172, 43, 174, byte.MaxValue),
-            new Color32(180, 62, 21, byte.MaxValue),
-            new Color32(195, 136, 34, byte.MaxValue),
-            new Color32(30, 31, 38, byte.MaxValue),
-            new Color32(132, 149, 192, byte.MaxValue),
-            new Color32(59, 23, 124, byte.MaxValue),
-            new Color32(56, 37, 16, byte.MaxValue),
-            new Color32(36, 168, 190, byte.MaxValue),
-            new Color32(21, 168, 66, byte.MaxValue),
-            //Twitch shadow color
-            //new Color32(86, 42, 150, byte.MaxValue),
-            new Color32(111, 45, 204, byte.MaxValue),
-            };
-            //this one is the one who loads the colors
-            //Palette.ShortColorNames = shortColorNames;
-            Palette.PlayerColors = playerColors;
-            Palette.ShadowColors = shadowColors;
-            //MedScanMinigame.ColorNames = colorNames;
-            //Telemetry.ColorNames = colorNames;
-
-            //a default code
+            //Used to get colors working
             RegisterInIl2CppAttribute.Register();
 
             Harmony.PatchAll();
@@ -285,6 +202,71 @@ namespace TestMod
                 {
                     source.RpcSetColor(PlayerControl_MurderPlayerPatch.origColor);
                 }
+            }
+        }
+
+        public static class CustomColorLoader
+        {
+            public class CustomColor
+            {
+                public StringNames shortColorName;
+                public StringNames fullColorName;
+                public Color32 mainColor;
+                public Color32 shadowColor;
+
+                public CustomColor(string shortColorName, string fullColorName, Color32 mainColor, Color32 shadowColor)
+                {
+                    this.shortColorName = CustomStringName.Register(shortColorName);
+                    this.fullColorName = CustomStringName.Register(fullColorName);
+                    this.mainColor = mainColor;
+                    this.shadowColor = shadowColor;
+                }
+            }
+
+            public static List<CustomColor> customColors = new List<CustomColor>();
+            
+            
+            private static void InitializeCustomColors()
+            {
+                CustomColor twitchColor = new CustomColor("TWCH", "Twitch", new Color32(164, 108, 243, byte.MaxValue), new Color32(111, 45, 204, byte.MaxValue));
+                customColors.Add(twitchColor);
+            }
+
+            public static void AddCustomColorsToGame()
+            {
+                InitializeCustomColors();
+
+                //Allocate array memory for custom colors
+                int colorCount = Palette.PlayerColors.Length;
+                UnhollowerBaseLib.Il2CppStructArray<Color32> playerColors = new UnhollowerBaseLib.Il2CppStructArray<Color32>(colorCount + customColors.Count);
+                UnhollowerBaseLib.Il2CppStructArray<Color32> shadowColors = new UnhollowerBaseLib.Il2CppStructArray<Color32>(colorCount + customColors.Count);
+                UnhollowerBaseLib.Il2CppStructArray<StringNames> shortColorNames = new UnhollowerBaseLib.Il2CppStructArray<StringNames>(colorCount + customColors.Count);
+                UnhollowerBaseLib.Il2CppStructArray<StringNames> fullColorNames = new UnhollowerBaseLib.Il2CppStructArray<StringNames>(colorCount + customColors.Count);
+
+                //Add orig colors to array
+                for(int i = 0; i < colorCount; i++)
+                {
+                    playerColors[i] = Palette.PlayerColors[i];
+                    shadowColors[i] = Palette.ShadowColors[i];
+                    shortColorNames[i] = Palette.ShortColorNames[i];
+                    fullColorNames[i] = Palette.ColorNames[i];
+                }
+
+                //Add cusotm colors to array
+                for (int i = 0; i < customColors.Count; i++)
+                {
+                    playerColors[colorCount + i] = customColors[i].mainColor;
+                    shadowColors[colorCount + i] = customColors[i].shadowColor;
+                    shortColorNames[colorCount + i] = customColors[i].shortColorName;
+                    fullColorNames[colorCount + i] = customColors[i].fullColorName;
+                }
+
+                //Update palette
+                Palette.PlayerColors = playerColors;
+                Palette.ShadowColors = shadowColors;
+                Palette.ShortColorNames = shortColorNames;
+                Palette.ColorNames = fullColorNames;
+                MedScanMinigame.ColorNames = fullColorNames; 
             }
         }
     }
