5de6df781fe2c4b8e14b76c659fa7ae37a173353
Efren Aguilar <efrenaguilar95@hotmail.com>
2021-03-27 15:12:12 -0700

    Turn off ghost swapping
    Maybe add this as an option later.

--- AmongUsModSrc/TwitchMod/ModManager.cs
+++ AmongUsModSrc/TwitchMod/ModManager.cs
@@ -197,23 +197,23 @@ namespace TwitchMod
             }
 
             List<string> playerNames = getPlayerNames();
-            List<string> deadPlayerNames = new List<string>();
+            //List<string> deadPlayerNames = new List<string>();
             playerNames.Shuffle();
             //Swap all living players
             for(int i = 0; i < playerNames.Count - 1; i++)
             {
                 if (playerInfoDict[playerNames[i]].IsDead)
                 {
-                    deadPlayerNames.Add(playerNames[i]);
+                    //deadPlayerNames.Add(playerNames[i]);
                     continue;
                 }
                 SwapPlayerPositions(playerControlDict[playerNames[i]], playerControlDict[playerNames[i + 1]]);
             }
             //Swap all ghost players
-            for (int i = 0; i < deadPlayerNames.Count - 1; i++)
-            {
-                SwapPlayerPositions(playerControlDict[deadPlayerNames[i]], playerControlDict[deadPlayerNames[i + 1]]);
-            }
+            //for (int i = 0; i < deadPlayerNames.Count - 1; i++)
+            //{
+            //    SwapPlayerPositions(playerControlDict[deadPlayerNames[i]], playerControlDict[deadPlayerNames[i + 1]]);
+            //}
             WriteToConsole("All players successfully swapped!");
         }
 
